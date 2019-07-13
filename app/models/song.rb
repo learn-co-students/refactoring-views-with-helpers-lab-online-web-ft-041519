afd63828c468cce760b0d2a8f41cce9920978033
Kish-Siva-1 <rodes88@msn.com>
2019-07-13 02:41:31 +0200

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name
   end
 
   def artist_name=(name)
+    if !Artist.find_by(name: name)
+      self.build_artist(name: name) 
+    else 
+      self.artist = Artist.find_by(name: name)
+    end 
   end
+
 end
